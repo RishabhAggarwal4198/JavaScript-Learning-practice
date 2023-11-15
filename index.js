3bde4bb18ba542b28c898ad4ec4eef6808e38de1
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-16 00:10:58 +0530

    docs: Changes in the documentation

---
+++ index.js
@@ -0,0 +1,23 @@
+
+console.log('Hello World !');
+
+
+let a = 12;
+let b = 20;
+
+a = b;
+
+console.log(a);
+
+
+let selectedColors = ['red', 'blue', 'green', 'white', 'blue'];
+selectedColors[2] = 1;
+
+console.log(selectedColors.length);
+
+
+function greet (name , lastName) {
+    console.log('Hello' + name + 'Your complete lastname is ' + lastName + ' right ?');
+}
+
+greet('Rakshita', 'Aggarwal');
