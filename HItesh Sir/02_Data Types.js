3cb909aaf2d8b5c13a4e8d09b20d1b7dd4ee2c0f
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-17 23:23:35 +0530

    docs: Changes in the documentation

--- HItesh Sir/02_Data Types.js
+++ HItesh Sir/02_Data Types.js
@@ -7,4 +7,55 @@ let name = "Rishabh" // String Data Type
 
 console.log(typeof null);
 
+
+
+
+
+
+
+
+// Primitive Data Types
+
+//  7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const value = "Hii Everyone";
+const socre = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 348242494898598989859895n;  // "BigInt" data type
+console.log(typeof bigNumber); // answer is "bigInt"
+
+
+
+
+// Refrence (Non - Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "dogs"]
+
+
+let myObj = {
+    name: "Rishabh",
+    age: 22
+}
+
+
+const myFunction = function () {
+    console.log("Hello world")
+}
+
+
+
+
+
   
