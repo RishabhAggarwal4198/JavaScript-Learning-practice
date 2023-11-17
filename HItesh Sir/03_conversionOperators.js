5a92c88ba42f474bfe29f8c85f774365fc69ae93
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-17 20:50:14 +0530

    docs: Changes in the documentation

--- HItesh Sir/03_conversionOperators.js
+++ HItesh Sir/03_conversionOperators.js
@@ -1,19 +1,19 @@
 
 let score = "33";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score); // for Conversion of the "score" variable into numbers
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 let score_2 = "44abc";
 
 console.log(typeof score_2);
 
 let valueInNumber_2 = Number (score_2);
-console.log(typeof valueInNumber_2);
-console.log(valueInNumber_2);  // it shows "NaN" i.e. NOT A NUMBER
+// console.log(typeof valueInNumber_2);
+// console.log(valueInNumber_2);  // it shows "NaN" i.e. NOT A NUMBER
 
 // "33" => 33
 // "33abc" => NaN
@@ -25,11 +25,41 @@ console.log(valueInNumber_2);  // it shows "NaN" i.e. NOT A NUMBER
 let isLoggedIn = 0;
 
 let booleanNumber = Boolean(isLoggedIn);
-console.log(typeof booleanNumber);
-console.log(booleanNumber);
+// console.log(typeof booleanNumber);
+// console.log(booleanNumber);
 
 // 1 => true; 0 => false
 // "" => false 
 // "Hitesh" => true
+
+
+
+
+// ************************* Operations *****************************
+
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2); 
+
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
 
 
