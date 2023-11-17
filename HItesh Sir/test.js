7cbe9baccc80c97a12dedefbc2ece348bcf538e2
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-17 14:16:27 +0530

    docs: Changes in the documentation

--- HItesh Sir/test.js
+++ HItesh Sir/test.js
@@ -1,1 +1,19 @@
 console.log("Rishabh Aggarwal");
+
+const accountId = 144553;
+let accountEmail = 'aggarwalrishabh4198@gmail.com';
+var accountPassword = '12345';
+accountCity = "Jaipur";
+
+
+
+// Changing the above variables
+
+accountEmail =  "rushikesh@gmail.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+
