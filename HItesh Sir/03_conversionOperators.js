216d722818e7dbde68e528e26c7541ca546d413f
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-17 18:14:03 +0530

    docs: Changes in the documentation

--- HItesh Sir/03_conversionOperators.js
+++ HItesh Sir/03_conversionOperators.js
@@ -3,3 +3,20 @@ let score = "33";
 
 console.log(typeof score);
 console.log(typeof(score));
+
+let valueInNumber = Number(score); // for Conversion of the "score" variable into numbers
+console.log(typeof valueInNumber);
+
+let score_2 = "44abc";
+
+console.log(typeof score_2);
+
+let valueInNumber_2 = Number (score_2);
+console.log(typeof valueInNumber_2);
+console.log(valueInNumber_2);  // it shows "NaN" i.e. NOT A NUMBER
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;  false => 0;
+
+
