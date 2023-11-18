654be410221faa7fd0811e0ca17084e090fa5ac7
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-18 14:11:52 +0530

    docs: Changes in the documentation

--- HItesh Sir/05_Strings.js
+++ HItesh Sir/05_Strings.js
@@ -25,8 +25,34 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 
 
+// Suppose we want to know that which character is on the position 't' in  String ('Rishabhhhhh'); ??
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
 
 
+
+
+const newString = gameName.substring(0,5)
+console.log(newString);   // result wont include the last digit number 
+
+const anotherString = gameName.slice(-10, 4);
+console.log(anotherString); 
+
+
+
+const newStringOne = "    Rishabh    "
+console.log(newStringOne);  // answer will be '    Rishabh    '
+console.log(newStringOne.trim()); // answer will be 'Rishabh'    [watch the documentation at mdn]
+
+
+
+
+
+const url = "https://rishabh.com//risabh%20aggarwal.com"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
 
 
 
