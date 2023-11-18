89f56de975b990ad317f05ef3a377e16dfecfc4e
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-18 23:07:06 +0530

    docs: Changes in the documentation

---
+++ HItesh Sir/07_Dates.js
@@ -0,0 +1,29 @@
+
+let myDate =  new Date ();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date (2023, 0, 23); 
+// console.log(myCreatedDate.toDateString());  // Answer is "Mon Jan 23 2023"
+
+
+let myCreatedDate2 = new Date (2024, 3, 25, 5, 3);
+// console.log(myCreatedDate2.toLocaleString()); // Answer is "25/4/2024, 5:03:00 am"
+
+
+let myCreatedDate3 = new Date ("01-14-2023");
+// console.log(myCreatedDate3.toLocaleString()); // Answer is "14/1/2023, 12:00:00 am"
+
+
+
+
+
+
+
+
+
+
+
