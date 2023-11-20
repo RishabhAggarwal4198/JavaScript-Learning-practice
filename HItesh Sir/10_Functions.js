1f6bac83b7e78755dcc4d48df677725f3cd7cac0
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-20 21:51:04 +0530

    docs: Changes in the documentation

---
+++ HItesh Sir/10_Functions.js
@@ -0,0 +1,25 @@
+
+// FUNCTIONS 👇👇
+
+function sayMyName () {
+    console.log("R");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("B");
+    console.log("H");
+}
+
+// sayMyName();   //  📍 Answer is  "R"  "I"  "S" "H" "A" "B" "H" 
+
+
+
+function addTwoNumbers (number1, number2) {
+     return number1**number2
+};
+
+
+const result = addTwoNumbers(3, 4);
+console.log(result);
+
