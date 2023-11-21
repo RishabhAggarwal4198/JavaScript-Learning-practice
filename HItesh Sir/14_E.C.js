a5430566dff438be908f4cffec8f6e51b41f359a
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-21 20:00:52 +0530

    docs: Changes in the documentation

---
+++ HItesh Sir/14_E.C.js
@@ -0,0 +1,14 @@
+
+/*
+ Types of EXECUTION CONTEXT :-
+ + Global Execution Context
+ + Function Execution Context
+ + Eval Execution Context
+ +
+
+ */ 
+
+
+
+
+//  (J.E.C) JavaScript Execution Context 
