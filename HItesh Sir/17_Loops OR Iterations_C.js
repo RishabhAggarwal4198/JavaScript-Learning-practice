68c3075cc86112e02ba964692c1ff3abe582c688
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-22 21:21:20 +0530

    docs: Changes in the documentation

--- HItesh Sir/17_Loops OR Iterations_C.js
+++ HItesh Sir/17_Loops OR Iterations_C.js
@@ -73,4 +73,4 @@ const shoppingCart = [
 
 const priceToPay = shoppingCart.reduce ((acc, item) => acc + item.price, 0);
 
-console.log(priceToPay);             //📍Answer is "22996"
+console.log(priceToPay);              //📍Answer is "22996"
