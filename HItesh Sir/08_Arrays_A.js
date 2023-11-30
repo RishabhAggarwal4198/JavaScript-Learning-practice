82b719fb59fbd9021407f361d293f087d3f24494
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-30 13:31:44 +0530

    docs: Changes in the documentation

--- HItesh Sir/08_Arrays_A.js
+++ HItesh Sir/08_Arrays_A.js
@@ -6,7 +6,7 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeros = ["Superman", "Batman", "Zoro"]
 // console.log(myHeros[1]);   // 📍 Answer is "Batman"
 
-// const myArr2 = new Array (1, 2, 3, 4) 
+const myArr2 = new Array (1, 2, 3, 4) 
 // console.log(myArr2[2]);   // 📍 Answer is "3"
 
 
@@ -23,15 +23,15 @@ myArr.push(7);
 
 
 myArr.pop()     // 📍 for REMOVINg the last value of the originla Array
-// console.log(myArr);   // 📍 Answer will be "[ 0, 1, 2, 3, 4, 5 ]"
+// console.log(myArr);   // 📍 Answer will be "[ 0, 1, 2, 3, 4, 5, 6 ]"
 
 
 myArr.unshift(9);    // 📍 For ADDING a value on to the 0th position of the Array
-// console.log(myArr);    // 📍  Answer is [ 9, 0, 1, 2, 3, 4, 5]
+// console.log(myArr);    // 📍  Answer is [ 9, 0, 1, 2, 3, 4, 5, 6]
 
 
 myArr.shift();      // 📍 For  REMOVING a value on to the 0th position of the Array
-// console.log(myArr);    // 📍  Answer is [ 0, 1, 2, 3, 4, 5 ]
+// console.log(myArr);    // 📍  Answer is [ 0, 1, 2, 3, 4, 5, 6 ]
 myArr.shift(); 
 // console.log(myArr);    // 📍  Answer is [ 1, 2, 3, 4, 5 ]
 
@@ -41,37 +41,39 @@ myArr.shift();
 // 📍 Answer is "false"
 
 
-console.log(myArr.indexOf());    // 📍For knowing whether given value of INDEX is in the original Array or not ?
-// 📍 Answer is "3"  
+// console.log(myArr.indexOf(4));    // 📍For knowing whether given value of INDEX is in the original Array or not ?
+// 📍 Answer is "3"     [it is talking its value from the last "myArr.shift()" operation]
 // console.log(myArr.indexOf(9));    // 📍For knowing whether given value of INDEX is in the original Array or not ?
 // 📍 Answer is "-1"  i.e. i don't know
 
 
 
-const newArr = myArr.join()     // converts all the values of an array into STRING
+const newArr = myArr.join()     // 📍adds all the elements of an array into STRING
 
-// console.log(myArr);     // 📍 Answer is "[ 1, 2, 3, 4, 5 ]"
-// console.log(newArr);    //  📍 Answer is "1,2,3,4,5" [as it got JOINED]
-
-
-
-
-
-// slice, splice
-
-// console.log("A ", myArr);  // 📍
-
-const myn1 = myArr. slice(1, 3)  // 📍 returns the value of  the ARRAY oo place 1 & 2 [3rd one is not icluded into it]
-
-// console.log(myn1);
-// console.log("B ", myArr);
+// console.log(myArr);     // 📍 Answer is "[ 1, 2, 3, 4, 5, 6 ]"
+// console.log(newArr);    //  📍 Answer is "1,2,3,4,5,6" [as it got JOINED & got converted to a STRING]
+// console.log(typeof newArr);   // 📍 Answer is "string"
 
 
 
 
-const myn2 = myArr. splice(1, 3)  // return the value of  the ARRAY oo place 1, 2 & 3 [and gonna DELETE that value from the original ARRAY]
-// console.log("C ", myArr ); //📍 Answer :- "C  [ 0, 4, 5 ]"  📍will DELETE those place values which are asked above in SPLICE
-// console.log(myn2); // 📍 Answer :- "[ 1, 2, 3 ]"
+
+
+
+
+// slice, splice 👇👇 
+
+// console.log("A ", myArr);  // 📍 Answer is "A  [ 1, 2, 3, 4, 5, 6 ]"
+
+const myn1 = myArr. slice(1, 3)  // 📍 returns the value of  the ARRAY of place 1 & 2 [3rd one is not icluded into it]
+
+// console.log(myn1);           // 📍 Answer is "[ 2, 3 ]"
+// console.log("B ", myArr);    // 📍 Answer is "B  [ 1, 2, 3, 4, 5, 6 ]"
+
+
+const myn2 = myArr. splice(1, 3)  // 📍return the value of  the ARRAY of place 1, 2 & 3 [and gonna DELETE that value from the original ARRAY]
+// console.log(myn2); // 📍 Answer is  "[ 2, 3, 4 ]"
+// console.log("C ", myArr ); //📍 Answer is  "C  [ 1, 5, 6 ]"  📍will DELETE those place values which are asked above in SPLICE
 
 
 
