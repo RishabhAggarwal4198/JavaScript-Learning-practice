1b3f1883f3de15b5108ec6ab2379f37fc83983b9
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-01 13:47:20 +0530

    docs: Changes in the documentation

--- HItesh Sir/13_IIFE.js
+++ HItesh Sir/13_IIFE.js
@@ -4,7 +4,7 @@ function game() {
     console.log(`DB CONNECTED`);
 }
 
-// game()    // 📍 Answer is "DB CONNECTED"
+// game()    // 📍 Answer is "DB CONNECTED" [V. V. Important @5:51:26]
 
 
                 // OR
@@ -31,3 +31,15 @@ function game() {
 // }) ("Rishabh");                           // 📍 Answer is "DB CONNECTED TWO Rishabh" [ 📍using ";" at the end is must ]
 
 
+
+
+
+
+/*  🚩🚩NOTE(for above)🚩🚩 
+✅  in this ()() [first parathesis "()" is where we gonna write the definition 
+    of the function & the second parathesis "()" is its "execution call"]
+
+✅  IIFEs are not only defined as fuction which immediatley gets executed but also
+     defined that sometime due to the pollution from Golbal scope so for removing the 
+     variables or declarations from global scope we use IIFE
+*/
