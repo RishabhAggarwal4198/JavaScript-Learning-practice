18ad6e74d3a1d32d8c1b413ed9ec8d40a0c30ec8
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-16 20:07:56 +0530

    docs: Changes in the documentation

---
+++ HItesh Sir/22_Small_Projects/2nd_Project/Project_2.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    
+})
+
+
