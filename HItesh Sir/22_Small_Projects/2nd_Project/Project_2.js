67beba75ef4db5d5a2212a426b30cea317534e5f
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-17 16:46:18 +0530

    docs: Changes in the documentation

--- HItesh Sir/22_Small_Projects/2nd_Project/Project_2.js
+++ HItesh Sir/22_Small_Projects/2nd_Project/Project_2.js
@@ -9,11 +9,14 @@ form.addEventListener('submit', function(e) {
       
 
     if(height === '' || height < 0 || isNaN (height) ){                 // [📍 '' = it should not be empty, isNaN(height) = if the value received is convertible to number & not a non numeric value. So it will display true otherwise false ]
-        results.innerHTML = `Please give a valid height ${height}`
+        results.innerHTML = `Please give a valid height ${height}`;
+    }else if(weight === '' || weight < 0 || isNaN (weight) ){                 
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    }else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
     }
-    results.innerHTML = `${height}`
-
-
+    
+   
 });
 
 
