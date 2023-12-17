4d3ae25d2634b88ff665abe91fd4662caf9707d7
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-17 13:53:08 +0530

    docs: Changes in the documentation

--- HItesh Sir/22_Small_Projects/2nd_Project/Project_2.js
+++ HItesh Sir/22_Small_Projects/2nd_Project/Project_2.js
@@ -6,10 +6,14 @@ form.addEventListener('submit', function(e) {
     const height = parseInt(document.querySelector('#height').value)     // [📍 by using "parseInt"  the value which we got in STRINGS got parsed into INTEGERS  ]
     const weight = parseInt(document.querySelector('#weight').value)
     const results= document.querySelector('#results')
-    
-    if(height === '' || height < 0 || isNaN(height) ){
-        results.innerHTML = "Please give a valid height"
+      
+
+    if(height === '' || height < 0 || isNaN (height) ){                 // [📍 '' = it should not be empty, isNaN(height) = if the value received is convertible to number & not a non numeric value. So it will display true otherwise false ]
+        results.innerHTML = `Please give a valid height ${height}`
     }
-}) 
+    results.innerHTML = `${height}`
+
+
+});
 
 
