1c7b0f969ce0beccedf048fa489c94b4479e127b
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-24 13:21:31 +0530

    docs: Changes in the documentation

---
+++ HItesh Sir/29_Promises_.js
@@ -0,0 +1,35 @@
+/*  For reading "PROMISE" documentation :-
+    ✅https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+    ✅https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+    */ 
+
+
+
+// 💎💎 Creating a PROMISE 👇👇
+
+const PromiseOne = new Promise (function(resolve, reject){     // [📍📍 in our daily world a PROMISE done by someone will only have two aspects 1) Either it will be completed 2) Or it will not get completed, So Similarly in JS also PROMISE have two aspects "resolve" & "rejected"]
+
+    // Do an Async task :-
+    // + DB calls
+    // + Cryptography
+    // + Network
+
+
+    setTimeout(function () {
+        console.log('ASync task is complete');
+    },1000)
+    
+})
+
+
+// 💎💎 Creating a PROMISE 👇👇
+    
+
+
+
+
+
+
+
+
