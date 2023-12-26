985dbd8ed836f9cdd03e920ac4e0f4d793e655a7
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-27 00:39:14 +0530

    docs: Changes in the documentation

---
+++ HItesh Sir/35_Inheritance_.js
@@ -0,0 +1,45 @@
+class User {
+    constructor (username) {
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+
+
+
+class Teacher extends User {
+    constructor (username, email, password) {
+        super(username)                        //[📍📍Using "super(username)" here will directly contact to the name of the class written after "extedds" & will call it, which in this case is "extends User"  ]
+        this.email = email
+        this.password = password
+    }
+
+    addCourse () {
+        console.log(`A new course was added by ${this.username}`);
+    }
+
+
+}
+
+const chayii = new Teacher ("Rohan", "rohan@gmail.com", "123");
+
+// chayii.addCourse()   //[📍📍 Answer is :-  "A new course was added by Rohan"  ]
+// chayii.logMe();       //[📍📍 Answer is :-  "USERNAME is Rohan"  ]
+
+
+
+const newChayii = new User("Rohan")
+
+// newChayii.logMe();  //[📍📍 Answer is :-  "USERNAME is Rohan"  ]
+// newChayii.addCourse();   //[📍📍 Answer is :-  Error  ]
+
+
+
+console.log(chayii === newChayii);    //[📍📍 Answer is :-  "false"  ]
+console.log(chayii === Teacher);     //[📍📍 Answer is :-  "false"  ]
+
+console.log(chayii instanceof User);   //[📍📍 Answer is :-  "true"  ]
