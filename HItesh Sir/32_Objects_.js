b0223d248a6765b82fd69cf1fd42484ccc415ac1
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-26 20:05:23 +0530

    docs: Changes in the documentation

--- HItesh Sir/32_Objects_.js
+++ HItesh Sir/32_Objects_.js
@@ -11,7 +11,7 @@ multiplyBy5.power = 2
 
 
 
-function createUser(username, score){
+function createUser(username, score){ 
     this.username = username
     this.score = score
 }
@@ -21,7 +21,39 @@ createUser.prototype.increment = function(){
     this.score++;
 }
 
-const chai = createUser("chai" , 25)
-console.log(chai); 
-const tea = createUser("tea", 250)
-console.log(tea);
+createUser.prototype.printMe = function () {
+    console.log(`score is ${this.score}`);
+}
+
+
+// const chai = createUser("chai" , 25)
+// console.log(chai);                   //[📍📍 Answer is :- "undefined" (bcoz there is no "new" keyword inserted) ]
+// const tea = createUser("tea", 250)
+// console.log(tea);                   //[📍📍 Answer is :- "undefined" ]
+
+
+                        // 🔽🔽(after inserting the "new" keyword)
+
+
+const chai = new createUser("chai" , 25)
+                //[📍📍 Answer is :- "undefined" (bcoz there is no "new" keyword inserted) ]
+const tea =  new createUser("tea", 250)
+                //[📍📍 Answer is :- "undefined" ]
+
+chai.printMe();     
+tea.increment();
+
+
+/*
+
+Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
