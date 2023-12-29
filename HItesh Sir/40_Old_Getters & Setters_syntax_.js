65564c8881969a35ff4fd14d562241589fec742e
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-12-29 15:10:24 +0530

    docs: Changes in the documentation

--- HItesh Sir/40_Old_Getters & Setters_syntax_.js
+++ HItesh Sir/40_Old_Getters & Setters_syntax_.js
@@ -9,9 +9,26 @@ function User (email, password){
             return this._email.toUpperCase()
         },
         set: function (value) {
-            this.email = value 
-        }
+            this._email = value 
+        } 
+    }) 
+
+
+    Object.defineProperty (this, 'password', {
+        
+        get: function () {
+            return this._password.toUpperCase()
+        },
+        set: function (value) {
+            this._password = value 
+        } 
     }) 
 }
+
+
+const infoo = new User ("ramesh@gmail.com", "Ramesh Raghav")
+
+console.log(infoo.email);                                   //[📍📍 Answer is :-  "RAMESH@GMAIL.COM"  ] 
+
 
  
