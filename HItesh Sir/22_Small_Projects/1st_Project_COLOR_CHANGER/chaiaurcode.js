b6af17ced29e29634e5030bbf36ef836703e924b
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-03 00:24:14 +0530

    docs: Changes in the documentation

--- HItesh Sir/22_Small_Projects/1st_Project_COLOR_CHANGER/chaiaurcode.js
+++ HItesh Sir/22_Small_Projects/1st_Project_COLOR_CHANGER/chaiaurcode.js
@@ -4,23 +4,32 @@ const body = document.querySelector('body')
 
 buttons.forEach(function (button){                          //[📍📍We got an "NodeList" over here (not an HTML collection)  that's why we used "ForEach" loop overe here ] 
     // console.log(button); 
-    button.addEventListener('click', function (e){
-        console.log(e); 
-        console.log(e.target);                              //💎💎✅💎💎 "e.target" gonna print the whole targeted button (DOM element) that was clicked   
-        if(e.target.id === 'grey'){ 
-            body.style.backgroundColor = e.target.id; 
+    button.addEventListener('click', function (event){
+        console.log(event); 
+        console.log(event.target);                              //💎💎✅💎💎 "event.target" gonna print the whole targeted button (DOM element) that was clicked   (Basically it tells us about from where this event is comming from ?)
+        if(event.target.id === 'grey'){                          //✅✅✅ is checks :- "in this 'event.target' the 'id' which we are getting , what is the vaule of it ?'
+            body.style.backgroundColor = event.target.id;       // 📍OR📍  body.style.backgroundColor = 'grey' 
         }   
-        if(e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id;
+        if(event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id;
         } 
-        if(e.target.id === 'blue'){ 
-            body.style.backgroundColor = e.target.id;
+        if(event.target.id === 'blue'){ 
+            body.style.backgroundColor = event.target.id;
         }
-        if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id;
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
         }
-        if(e.target.id === 'Red'){
-            body.style.backgroundColor = e.target.id;
+        if(event.target.id === 'Red'){
+            body.style.backgroundColor = event.target.id;
+        } 
+        if(event.target.id === 'Orange'){
+            body.style.backgroundColor = event.target.id;
+        } 
+        if(event.target.id === 'Purple'){
+            body.style.backgroundColor = event.target.id;
+        } 
+        if(event.target.id === 'Sky_Blue'){
+            body.style.backgroundColor = 'skyblue';
         } 
     }) 
 });
