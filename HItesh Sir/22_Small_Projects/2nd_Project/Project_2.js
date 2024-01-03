d56355d9e8e735ee0ca61ca5de6119e7b2942d99
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-03 11:00:24 +0530

    docs: Changes in the documentation

--- HItesh Sir/22_Small_Projects/2nd_Project/Project_2.js
+++ HItesh Sir/22_Small_Projects/2nd_Project/Project_2.js
@@ -1,16 +1,18 @@
 const form = document.querySelector('form')
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
+// const height = parseInt(document.querySelector('#height').value)        //[📍📍This useCase will cause an empty value to get stored inside the " const height" ] 
 
-    const height = parseInt(document.querySelector('#height').value)     // [📍 by using "parseInt"  the value which we got in STRINGS got parsed into INTEGERS  ]
-    const weight = parseInt(document.querySelector('#weight').value)
+form.addEventListener('submit', function(e) {
+    e.preventDefault()                                                    //💎✅💎 For preventing the form from default submission (i.e. from preventing it to perform default action associated with it )
+
+    const height = parseInt(document.querySelector('#height').value)        // [✅📍 by using ".value" we gets the value of which is entered in the input by the user ]
+    const weight = parseInt(document.querySelector('#weight').value)      // [✅📍 the value which we get from this "document.querySelector('#weight').value" is STRING so, by using "parseInt"  the value which we got in STRINGS got parsed into INTEGERS  ]
     const results= document.querySelector('#results')
     const Suggestion = document.querySelector('#Suggestion')
       
 
-    if(height === '' || height < 0 || isNaN (height) ){                 // [📍 '' = it should not be empty, isNaN(height) = if the value received is convertible to number or if it's a numeric value. So it will display true otherwise false ]
-        results.innerHTML = `Please give a valid height ${height}`;
+    if(height === '' || height < 0 || isNaN (height) ){                 // [📍 '' = it should not be empty, isNaN(height) = if the value received is convertible to number or if it's a numeric value. So it will display true otherwise false (i.e. height = NaN (not a number)) ]
+        results.innerHTML = `Please give a valid height !! Given value is ${height}`;
     }else if (weight === '' || weight < 0 || isNaN (weight) ){                 
         results.innerHTML = `Please give a valid weight ${weight}`;
     }else {
