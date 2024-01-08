5119670a89e42309d7c0629d6740c15cb9c3f3c7
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-08 19:46:33 +0530

    docs: Changes in the documentation

--- HItesh Sir/39_Getters & Setters_.js
+++ HItesh Sir/39_Getters & Setters_.js
@@ -17,25 +17,27 @@
                                     
 // 💎✅💎GETTERS & SETTERS (from pereventing someone accessing "password") 👇👇
                                     
-// class User {
-//     constructor (email, password) {
-//         this.email = email;
-//         this.password = password;
-//     }
+class User {
+    constructor (email, password) {
+        this.email = email;
+        this.password = password;
+    }
 
-//     get password () {
-//         return this._paSSword.toUpperCase()
-//     }
+    get password () {
+        return this._paSSword.toUpperCase()
+    }
 
-//     set password(value) {
-//         this._paSSword = value.toUpperCase()
-//     }
-// }
+    set password(value) {
+        this._paSSword = value.toUpperCase()
+    }
+}
 
 
 
-// const Ramesh = new User ("ramesh@gmail.com", "asdf_vbnm")
-// console.log(Ramesh.password);                              //  [📍📍 Answer is :- "ASDF_VBNM" ] 
+
+
+const Ramesh = new User ("ramesh@gmail.com", "asdf_vbnm")
+console.log(Ramesh.password);                              //  [📍📍 Answer is :- "ASDF_VBNM" ] 
 
 
 
