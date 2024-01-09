5e48cb88b319edbc31b112ff95a2980f17a827b1
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-09 22:51:58 +0530

    docs: Changes in the docuementation

--- HItesh Sir/05_Strings.js
+++ HItesh Sir/05_Strings.js
@@ -2,47 +2,73 @@ const name = "Rishabh"
 
 const repoCount = 50
 
-
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
-
-
-
-// Another way of DECLARING string
-
-const gameName = new String ('Rishabhhhhh');
-
-console.log(gameName);
-
-// For accessing the prototype stored in above VARIABLE
-console.log(gameName[0]);              // 📍 Answer is "R"
-console.log(gameName[4]);              // 📍 Answer is "a"
+// ❌❌👇❌❌Primitive way of CONCATENATION❌❌👇❌❌
+// console.log(name + repoCount + "value");     //[📍📍 Answer is :-  "Rishabh50value"  ] 
 
 
-console.log(gameName.__proto__);       // 📍 Answer is "{}"
-console.log(gameName.length);          // 📍 Answer is "11"
-console.log(gameName.toUpperCase());
-console.log(gameName.trimEnd(2));
+// ✅✅👇✅✅STRING INTERPOLATION - modern way of CONCATENATION✅✅👇✅✅
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);    //[📍📍 Answer is :-  "Hello my name is Rishabh and my repo count is 50"  ] 
 
 
-// Suppose we want to know that which character is on the position 't' in  String ('Rishabhhhhh'); ??
-console.log(gameName.charAt(2));       // 📍 Answer is "s"
-console.log(gameName.indexOf('h'));    // 📍 Answer is "3"
+
+
+// 💎✅💎Another way of DECLARING string👇👇 
+
+const gameName = new String ('Rishabhhhhh');   // [@2:12:06 ]
+
+/* Print "gameName" in console & you will get :-
+ * String {'Rishabhhhhh'}
+        0: "R"
+        1: "i"
+        2: "s"
+        3: "h"
+        4: "a"
+        5: "b"
+        6: "h"
+        7: "h"
+        8: "h"
+        9: "h"
+        10: "h"
+        length: 11
+        [[Prototype]]: String[[PrimitiveValue]]: "Rishabhhhhh"
+ */
+
+// console.log(gameName);        //[📍📍 Answer is :-  "[String: 'Rishabhhhhh']"  ] 
+
+
+
+
+// / 💎✅💎For accessing the prototype stored in above VARIABLE👇👇
+
+// console.log(gameName[0]);              // 📍 Answer is "R"
+// console.log(gameName[4]);              // 📍 Answer is "a"
+
+// console.log(gameName.__proto__);       // 📍 Answer is "{}"
+// console.log(gameName.length);          // 📍 Answer is "11"
+// console.log(gameName.toUpperCase());      // 📍 Answer is "RISHABHHHHH"
+// console.log(gameName.trimEnd(2));
+
+// 💎✅💎 Suppose we want to know that which character is on the position 't' in  String ('Rishabhhhhh'); ?? 👇👇
+// console.log(gameName.charAt(2));       // 📍 Answer is "s"
+
+// 💎✅💎 Suppose we want to know that at which position is on the character 'h' in  String ('Rishabhhhhh'); ?? 👇👇
+// console.log(gameName.indexOf('h'));    // 📍 Answer is "3"
 
 
 
 
 
 const newString = gameName.substring(0,5)     // 📍 Answer is "Risha"  [the last value 5 is not icluded]
-console.log(newString);   // result wont include the last digit number 
+// console.log(newString);   // result wont include the last digit number 
 
 const anotherString = gameName.slice(-10, 4);    // 📍📍📍📍 [MUST SEE] Run the program to see
-console.log(anotherString); 
-
+// console.log(anotherString); 
+// 
 
 
 const newStringOne = "    Rishabh    "
-console.log(newStringOne);                   // 📍📍 answer will be '    Rishabh    '
-console.log(newStringOne.trim());            // 📍📍 answer will be 'Rishabh'  [ it will remove the space at the startng & end ]   [watch the documentation at mdn]
+// console.log(newStringOne);                   // 📍📍 answer will be '    Rishabh    '
+// console.log(newStringOne.trim());            // 📍📍 answer will be 'Rishabh'  [ it will remove the space at the startng & end ]   [watch the documentation at mdn]
 
 
 
@@ -50,9 +76,9 @@ console.log(newStringOne.trim());            // 📍📍 answer will be 'Rishabh
 
 const url = "https://rishabh.com//risabh%20aggarwal.com"
 
-console.log(url.replace('%20', '-'))       // 📍 Answer will be  "https://rishabh.com//risabh-aggarwal.com" [ will remove "20%" at insert "-" in place of it]
+// console.log(url.replace('%20', '-'))       // 📍 Answer will be  "https://rishabh.com//risabh-aggarwal.com" [ will remove "20%" at insert "-" in place of it]
 
-console.log(url.includes('sundar'))      // 📍 Answer will be  "false" [Will tell if there would be any such word in the oroginal string]
+// console.log(url.includes('sundar'))      // 📍 Answer will be  "false" [Will tell if there would be any such word in the oroginal string]
 
 
 
@@ -60,7 +86,7 @@ console.log(url.includes('sundar'))      // 📍 Answer will be  "false" [Will t
 
 let value = "I went to a nice hill station."
 
-console.log(value.split('a'));
+// console.log(value.split('a'));
 
 
 
