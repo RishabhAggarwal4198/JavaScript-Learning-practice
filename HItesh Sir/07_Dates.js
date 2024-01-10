895c3830d13960c2b5c6dbf950677896f22368e6
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-10 15:01:23 +0530

    docs: Changes in the docuementation

--- HItesh Sir/07_Dates.js
+++ HItesh Sir/07_Dates.js
@@ -20,7 +20,7 @@ let myCreatedDate2 = new Date (2024, 3, 25, 5, 3); //✅ [Here "3" is the 4rth m
 
 
 let myCreatedDate3 = new Date ("2023-01-14");   //📍 [YYYY-MM-DD  format]
-console.log(myCreatedDate3.toLocaleString());   //📍 Answer is "14/1/2023, 12:00:00 am"
+// console.log(myCreatedDate3.toLocaleString());   //📍 Answer is "14/1/2023, 12:00:00 am"
 
                                      // 🔽🔽
 
@@ -29,29 +29,35 @@ let myCreatedDate4 = new Date ("01-14-2023");   //📍 [MM-DD-YYYY  format]
 
 
 
-// When u want values between two NUMBERS only 👇 
+//✅ When u want values between two NUMBERS only 👇 
 
+let myCreatedDate5 = new Date (2023, 0, 23); 
 let myTimeStamp = Date.now();
 
-// console.log(myTimeStamp);       // 📍 Answer is "1701247039259" [its the millisecond value from 1st January 1970 uptill now]
-// console.log(myCreatedDate.getTime());    // 📍 Answer is "1674412200000" [its the millisecond value from 01-14-2023 uptill now]
+// console.log(myTimeStamp);       // 📍 Answer is "1701247039259" miliseconds [its the millisecond value from 1st January 1970 uptill now]
+// console.log(myCreatedDate5.getTime());    // 📍 Answer is "1674412200000" miliseconds  [its the millisecond value from "1st January 1970" uptill "Mon Jan 23 2023" ]
 
 
 
-// console.log(Date.now());        // 📍 Answer is "1701247516567"
+// 💎✅💎 To convert miliseconds value into seconds 👇👇
+// console.log(Date.now());        // 📍 Answer is "1704878426710"
+// console.log(Math.floor(Date.now()/1000)); // 📍 Answer will be "1704878426" seconds
 
-// console.log(Math.floor(Date.now()/10000000000)); // 📍 Answer will be "170" seconds
 
 
 
+// 💎✅💎  👇👇
 
 let newDate = new Date (2023, 2 , 22);
-// console.log(newDate.getMonth() + 1);
-// console.log(newDate.getDay()); 
+// console.log(newDate.getMonth());           // [📍📍 Answer is :-  "2" (that is month "March")  ] 
+// console.log(newDate.getMonth()+1);          // ✅✅ "+1" to prevent user form getting confused
+// console.log(newDate.getDay());              // [📍📍 Answer is :-  "3" (that is "Wednesday")  ] 
 
 // console.log(`${newDate.getDay()} is the time`);  
 
-newDate.toLocaleString('default', {
+
+
+newDate.toLocaleString('default', {            //💎💎[@3:08:53]
     weekday: "long"
 })
 
