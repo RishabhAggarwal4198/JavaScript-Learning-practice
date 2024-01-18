815bb5ea365259df3374dcdb7fbb7e313c0e4719
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-18 23:55:52 +0530

    docs: Changes in the docuementation

--- HItesh Sir/My_Projects/1st_Project_.js
+++ HItesh Sir/My_Projects/1st_Project_.js
@@ -10,4 +10,5 @@ document.querySelector('#File').addEventListener('click', function () {
 document.querySelector('#Insert').addEventListener('click', function(){
     document.querySelector('#doc').style.height="500px"
     document.querySelector('#doc').style.width="300px"
+    document.querySelector('#doc').style.backgroundColor="Pink"
 })
