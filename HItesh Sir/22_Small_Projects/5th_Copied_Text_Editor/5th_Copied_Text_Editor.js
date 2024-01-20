ae753c0964faec994050a975adeb933d4f484756
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-20 19:42:50 +0530

    docs: Changes in the docuementation

--- HItesh Sir/22_Small_Projects/5th_Copied_Text_Editor/5th_Copied_Text_Editor.js
+++ HItesh Sir/22_Small_Projects/5th_Copied_Text_Editor/5th_Copied_Text_Editor.js
@@ -2,7 +2,6 @@ function formatDoc ( cmd, value=null) {
 
         if(value){
                 document.execCommand(cmd, false , value);  
-                console.log(document.execCommand(cmd, false , value));    /* ❓❓❓ */   
         } else {
                 document.execCommand(cmd);   
         } 
