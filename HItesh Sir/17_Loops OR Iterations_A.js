463caf5ece3baf6ad78ff82d502cf3354a38a5a2
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-29 11:25:05 +0530

    docs: Changes in the docuementation

--- HItesh Sir/17_Loops OR Iterations_A.js
+++ HItesh Sir/17_Loops OR Iterations_A.js
@@ -294,7 +294,12 @@ while (arr < myArray.length) {
 let fruits = [`Mango`, `Apple`, `Banana`, `Litchi`, `Orange`]
 
 for(let i = 0; i < fruits.length; i++){
-   console.log(fruits[i]);
+//    console.log(fruits[i]);
+}
+
+// In Reverse
+for(let i = fruits.length-1; i >= 0; i-- ){
+	console.log(fruits[i]);
 }
 
 
