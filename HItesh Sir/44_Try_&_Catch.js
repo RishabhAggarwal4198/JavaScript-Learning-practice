471ecd0462d9d7d64e0549fb57d44de84434f829
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-01-30 22:28:28 +0530

    docs: Changes in the docuementation

---
+++ HItesh Sir/44_Try_&_Catch.js
@@ -0,0 +1,14 @@
+
+console.log("Hello");
+console.log("Hello");
+
+try {
+  console.log(a);	
+} catch (err){
+  console.log('Caught an error');
+  console.log(err);
+}
+
+console.log("Semal");
+console.log("Semal");
+console.log("Semal");
