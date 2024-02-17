58381547045d9a8704cd1c13089e42ae2a630fc7
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-17 14:07:42 +0530

    docs: Changes in the docuementation

--- HItesh Sir/131_🍁💎_PhotoEditor_.js
+++ HItesh Sir/131_🍁💎_PhotoEditor_.js
@@ -1,6 +1,8 @@
 let  choose_img_Btn = document.querySelector('.choose_img  button')
 let choose_Input = document.querySelector('.choose_img input')
 let imgSrc = document.querySelector('.view_img img');
+let filter_buttons = document.querySelectorAll('.icons-room  button')
+
 
 
 choose_img_Btn.addEventListener('click', () => choose_Input.click());  
@@ -10,8 +12,17 @@ choose_Input.addEventListener('change', () => {
 	let file = choose_Input.files[0];
 	if(!file) return;           //✅✅If 'file' doesn't exist return nothing
 	imgSrc.src = URL.createObjectURL(file);
+
+	// document.querySelector('.container').classList.remove('disabled')
+								// 🔽🔽
 	imgSrc.addEventListener('load', () => {
 		document.querySelector('.container').classList.remove('disabled')
 	})
 })
 
+
+filter_buttons.forEach((element) => {
+	element.addEventListener('click', () => {
+		 
+	})
+})
