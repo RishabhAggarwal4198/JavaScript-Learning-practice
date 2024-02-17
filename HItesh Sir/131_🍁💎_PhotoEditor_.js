3dfcc3a2f5800a665649c3f04c74c8ee0a5f5106
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-17 14:26:17 +0530

    docs: Changes in the docuementation

--- HItesh Sir/131_🍁💎_PhotoEditor_.js
+++ HItesh Sir/131_🍁💎_PhotoEditor_.js
@@ -24,6 +24,6 @@ choose_Input.addEventListener('change', () => {
 filter_buttons.forEach((element) => {
 	element.addEventListener('click', () => {
 		 document.querySelector(".active").classList.remove("active");
-		 element.classList.add("active")
+		 element.classList.add("active");
 	});
 })
