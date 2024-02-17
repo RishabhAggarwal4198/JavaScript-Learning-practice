0090204e4217f7c9e17aaa49c2c554f9f2458ef9
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-17 12:50:08 +0530

    docs: Changes in the docuementation

---
+++ HItesh Sir/131_🍁💎_PhotoEditor_.js
@@ -0,0 +1,4 @@
+let  choose_img_Btn = document.querySelector('.choose_img  button')
+let choose_Input = document.querySelector('.choose_img input')
+
+choose_img_Btn.addEventListener('click', () => choose_Input.click());
