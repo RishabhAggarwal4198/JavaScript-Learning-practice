10f645da090819203da59b42fc031f66145e66d6
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-17 13:19:10 +0530

    docs: Changes in the docuementation

--- HItesh Sir/131_🍁💎_PhotoEditor_.js
+++ HItesh Sir/131_🍁💎_PhotoEditor_.js
@@ -5,6 +5,11 @@ let imgSrc = document.querySelector('.view_img img');
 
 choose_img_Btn.addEventListener('click', () => choose_Input.click());  
 choose_Input.addEventListener('change', () => {
-     
+
+    //  console.log(choose_Input.files[0]); 
+	let file = choose_Input.files[0];
+	if(!file) return;           //✅✅If 'file' doesn't exist return nothing
+	imgSrc.src = URL.createObjectURL(file);
+
 })
 
