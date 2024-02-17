3bd5ce98e497798037f171c16abc6ced97f22aef
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-17 13:03:06 +0530

    docs: Changes in the docuementation

--- HItesh Sir/131_🍁💎_PhotoEditor_.js
+++ HItesh Sir/131_🍁💎_PhotoEditor_.js
@@ -1,4 +1,9 @@
 let  choose_img_Btn = document.querySelector('.choose_img  button')
 let choose_Input = document.querySelector('.choose_img input')
+let imgSrc = document.querySelector('.view_img img');
+
 
 choose_img_Btn.addEventListener('click', () => choose_Input.click());  
+choose_Input.addEventListener('change', () => {
+	console.log(5 + 5);
+})
