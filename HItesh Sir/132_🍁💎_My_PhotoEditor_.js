7376f0407689405832505a92cf13fdb56aba84d7
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-19 17:11:35 +0530

    docs: Changes in the docuementation

--- HItesh Sir/132_🍁💎_My_PhotoEditor_.js
+++ HItesh Sir/132_🍁💎_My_PhotoEditor_.js
@@ -1,6 +1,14 @@
 let uploadBtn = document.querySelector('#uploaD button')
 let uploadInput = document.querySelector('#uploaD input')
+let focusImage = document.querySelector('.image_div  img')
 
 
 
 uploadBtn.addEventListener('click', () => uploadInput.click());
+
+uploadInput.addEventListener('change', () => {
+	let newUrl = URL.createObjectURL(uploadInput.files[0])
+	focusImage.src = newUrl
+	focusImage.classList.remove('mainIMG')
+	focusImage.classList.add('NewImg')
+})
